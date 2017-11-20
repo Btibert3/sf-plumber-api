4e16574585c03e33880b02ca0cecdf1b1909de59
Brock <btibert3@gmail.com>
2017-11-19 23:24:28 -0500

    tweaks

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ABOUT
 
-Post showing how to create an api in R and consume that api in Salesforce to score a record
+Post showing how to create an api in R and consume that api in Salesforce to score a record.
+
+https://releasenotes.docs.salesforce.com/en-us/spring17/release-notes/rn_forcecom_external_services_about.htm
+
--- main.R
+++ main.R
@@ -2,10 +2,11 @@ options(stringsAsFactors = FALSE)
 
 ## load the libraries
 library(plumber)
+library(analogsea)
 
 ## manual way locally
-r = plumber::plumb("api/plumber.R")
-r$run(port = 8000, swagger = TRUE)
+# r = plumber::plumb("api/plumber.R")
+# r$run(port = 8000, swagger = TRUE)
 
 
 ## CANT GET DO to WORK, what is the code to spin up docker/git setup
@@ -18,11 +19,13 @@ r$run(port = 8000, swagger = TRUE)
 # analogsea::droplets()
 
 ## initialize a droplet
-# mydrop = plumber::do_provision()
-# 
-# ## update the packages
+mydrop = plumber::do_provision()
+# may error out on port 22
+
+
+# # ## update the packages
 # analogsea::droplets()
-# analogsea::install_r_package(mydrop$id)
-# 
-# ## deploy the api
-# plumber::do_deploy_api(mydrop$id, "admitscore", "./api/plumber.r", "8000")
+analogsea::install_r_package(mydrop$id)
+
+# ## deploy the api (from the home directory)
+plumber::do_deploy_api(mydrop$id, "admitscore", "./api/", "8001", preflight = "swagger=TRUE")
